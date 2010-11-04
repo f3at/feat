8c1ca95135186776e6dc5c8456c4dee4616f7cfa
Sebastien Merle <s.merle@gmail.com>
2010-11-04 18:43:26 +0100

    Changed states

--- src/feat/interface/contracts.py
+++ src/feat/interface/contracts.py
@@ -7,32 +7,41 @@ class ContractState(enum.Enum):
     '''Contract protocol state:
 
     For manager:
-     - initiated: FIXME: blah blah
-     - announced: For managers, the manager published an announcement
-     to contractors.
-     - closed: For managers, the contract has been closed because it expired
-     or a response has been received from all contractors. For contractors,
-     announce expired without putting bid nor refusal.
-     - bid: Only for contractors. A bid has been put on an announcement.
-     - rejected: Only for contractors. The bid has been rejected by the manager.
-     - granted: The contract has been granted to one or multiple contractor.
-     - acknowledged: The contract has been acknowledged by the manager.
-     - cancelled: The contract got aborted because of one of the peer failure.
+
+     - initiated:    The manager has been initiated but didn't publish any
+                      announcement yet.
+     - announced:    The manager published an announcement to contractors.
+     - closed:       The contract has been closed because the announce expired
+                      or a response has been received from all involved
+                      contractors.
+     - granted:      The contract has been granted to one or multiple
+                      contractor.
+     - expired:      Contract expire without granting any bid.
+     - cancelled:    Some contractor didn't report in time so all contractors
+                      got cancelled.
+     - acknowledged: All granted jobs have been acknowledged by the manager.
+     - aborted:      Some jobs have been cancelled so all the contractors
+                      got cancelled.
+     - bid:          Only for contractors, invalid state for managers.
+     - refused:      Only for contractors, invalid state for managers.
+     - rejected:     Only for contractors, invalid state for managers.
+     - completed:    Only for contractors, invalid state for managers.
 
     For contractor:
-     - initiated: The instance is created, the announce message is not
-                  parsed yet.
-     - announced: The manager published an announcement
-     - closed: The Announce expired without putting bid nor refusal.
-     - bid: A bid has been put on an announcement.
-     - refused: The announce has been refused.
-     - rejected: The bid has been rejected by the manager.
-     - granted: The contract has been granted.
-     - expired: Bid expired without grant nor rejection.
-     - completed: Granted job is completed.
-     - cancelled: The manager cancelled the granted job.
-     - acknowledged: the manager acknowledged the completed job.
-     - aborted: The manager has not acknowledged the report in time.
+     - initiated:    The contractor is created, the announce message is not
+                      parsed yet.
+     - announced:    The manager published an announcement
+     - closed:       The Announce expired without putting bid nor refusal.
+     - bid:          A bid has been put on an announcement.
+     - refused:      The announce has been refused.
+     - rejected:     The bid has been rejected by the manager.
+     - granted:      The contract has been granted.
+     - expired:      Bid expired without grant nor rejection.
+     - completed:    Granted job is completed.
+     - cancelled:    The manager cancelled the job.
+     - acknowledged: The manager acknowledged the completed job.
+     - aborted:      The manager has not acknowledged the report in time,
+                      or explicitly canceled the job.
     '''
     (initiated, announced, closed, bid, refused, rejected, granted,
      expired, completed, cancelled, acknowledged, aborted) = range(12)
