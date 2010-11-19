427651449ffff786dac7ca69bb61cce9363445a8
Marek Kowalski <mkowalski@flumotion.com>
2010-11-19 12:34:23 +0100

    Remove obsolete method from IAgency interface

--- src/feat/interface/agency.py
+++ src/feat/interface/agency.py
@@ -11,11 +11,6 @@ class IAgency(Interface):
     def start_agent(factory, descriptor, *args, **kwargs):
         '''Start new agent from factory. Returns the L{IAgencyAngent}'''
 
-    def callLater(timeout, method, *args, **kwargs):
-        '''
-        Wrapper for reactor.callLater.
-        '''
-
     def get_time():
         '''
         Use this to get current time. Should fetch the time from NTP server
