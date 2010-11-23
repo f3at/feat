6b18f772808607ab9903d9416ed1152c5dff289d
Sebastien Merle <s.merle@gmail.com>
2010-11-23 18:03:58 +0100

    Added test for meta errors

--- src/feat/test/test_common_reflect.py
+++ src/feat/test/test_common_reflect.py
@@ -7,6 +7,68 @@ from feat.common import reflect
 from . import common
 
 
+def test_depth2(depth=2):
+    return  reflect.class_locals(depth)
+
+
+def test_depth3(depth=3):
+    return  test_depth2(depth=depth)
+
+
+try:
+    meta_test1_pass = True
+
+    class MetaError1(object):
+
+        class_local = True
+
+        if "class_local" not in reflect.class_locals(1):
+            raise RuntimeError()
+
+        if "class_local" not in test_depth2():
+            raise RuntimeError()
+
+        if "class_local" not in test_depth3():
+            raise RuntimeError()
+
+except RuntimeError:
+    meta_test1_pass = False
+
+
+try:
+    meta_test2_pass = False
+
+    class MetaError2(object):
+
+        if "class_local" not in test_depth2(depth=3):
+            raise RuntimeError()
+
+except TypeError, e:
+    meta_test2_pass = True
+
+
+try:
+    meta_test3_pass = False
+
+    class MetaError2(object):
+
+        if "class_local" not in test_depth2(depth=1):
+            raise RuntimeError()
+
+except TypeError:
+    meta_test3_pass = True
+
+
+meta_test4_pass = not reflect.inside_class_definition(1)
+
+
+class MetaError3(object):
+
+    bad1 = reflect.inside_class_definition(0)
+    good = reflect.inside_class_definition(1)
+    bad2 = reflect.inside_class_definition(2)
+
+
 class Dummy(object):
 
     def spam(self):
@@ -18,6 +80,17 @@ def bacon():
 
 
 class TestIntrospection(common.TestCase):
+
+    class_local = True
+
+    def testMetaErrors(self):
+        self.assertTrue(meta_test1_pass)
+        self.assertTrue(meta_test2_pass)
+        self.assertTrue(meta_test3_pass)
+        self.assertTrue(meta_test4_pass)
+        self.assertFalse(MetaError3.bad1)
+        self.assertTrue(MetaError3.good)
+        self.assertFalse(MetaError3.bad2)
 
     def testClass(self):
         self.assertEqual("feat.test.test_common_reflect.Dummy",
@@ -47,3 +120,8 @@ class TestIntrospection(common.TestCase):
 
     def testNone(self):
         self.assertEqual(None, reflect.canonical_name(None))
+
+#    def testNotInClass(self):
+#        self.assertRaises(TypeError, reflect.class_locals, 1)
+#        self.assertRaises(TypeError, reflect.class_locals, 2)
+#        self.assertTrue("class_local" in reflect.class_locals(4))
