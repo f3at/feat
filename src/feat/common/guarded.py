4d9f531c0f874a8c2280db0990bcd676353415d1
Sebastien Merle <s.merle@gmail.com>
2010-11-24 18:14:30 +0100

    Added immutable decorator with state freeze and unit tests, but not implemented it yet

--- src/feat/common/guarded.py
+++ src/feat/common/guarded.py
@@ -1,6 +1,8 @@
 # -*- Mode: Python -*-
 # vi:si:et:sw=4:sts=4:ts=4
 
+import types
+
 from zope.interface import implements
 
 from feat.interface.serialization import *
@@ -10,11 +12,30 @@ from . import decorator, annotate, serialization
 STATE_TAG = "_GUARDED_STATE_"
 
 
+def freeze(value):
+    '''Not Yet Implemented'''
+    return value
+
+
 @decorator.simple_function
 def mutable(function):
+    '''Add the instance internal state as the second parameter
+    of the decorated function.'''
 
     def wrapper(self, *args, **kwargs):
         state = self._get_state()
+        return function(self, state, *args, **kwargs)
+
+    return wrapper
+
+
+@decorator.simple_function
+def immutable(function):
+    '''Add the instance internal state as the second parameter
+    of the decorated function.'''
+
+    def wrapper(self, *args, **kwargs):
+        state = freeze(self._get_state())
         return function(self, state, *args, **kwargs)
 
     return wrapper
@@ -26,13 +47,14 @@ class MutableState(serialization.Serializable):
 
 class Guarded(serialization.Serializable):
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         state = MutableState()
         setattr(self, STATE_TAG, state)
-        self.init_state(state)
+        self.init_state(state, *args, **kwargs)
 
-    def init_state(self, state):
-        '''Override to initialize the state.'''
+    def init_state(self, state, *args, **kwargs):
+        '''Override to initialize the state. The extra arguments
+        and keywords are the one passed to the constructor.'''
 
     def recover(self, snapshot):
         setattr(self, STATE_TAG, snapshot)
--- src/feat/test/test_common_guarded.py
+++ src/feat/test/test_common_guarded.py
@@ -7,6 +7,7 @@ from feat.common import guarded
 from feat.interface.serialization import *
 
 from . import common
+from twisted.trial.unittest import SkipTest
 
 
 class Dummy(guarded.Guarded):
@@ -22,12 +23,36 @@ class Dummy(guarded.Guarded):
         state.value += result
         return result
 
-    @guarded.mutable
+    @guarded.immutable
     def get_value(self, state):
         return state.value
 
 
-class TestIntrospection(common.TestCase):
+class FreezeDummy(object):
+
+    def __init__(self):
+        self.simple_bool = True
+        self.simple_int = 42
+        self.simple_str = "dummy"
+        self.simple_list = [1, 2, 3]
+        self.simple_tuple = (2, 3, 4)
+        self.simple_dict = {7: 14, 8: 16, 9: 18}
+        self.simple_set = set([1, 5, 9])
+
+        self.combined_list = [1, [2, 3], 4]
+        self.combined_tuple = (1, [2, 3], 4)
+
+
+class TopFreezeDummy(FreezeDummy):
+
+    def __init__(self):
+        FreezeDummy.__init__(self)
+        self.reference = FreezeDummy()
+        self.list_ref = [1, FreezeDummy(), 3]
+        self.tuple_ref = (1, FreezeDummy(), 3)
+
+
+class TestStateGuard(common.TestCase):
 
     def testBasicGuarded(self):
         obj = Dummy()
@@ -48,3 +73,181 @@ class TestIntrospection(common.TestCase):
         self.assertEqual(obj2.get_value(), 7)
         self.assertEqual(obj.double(2, minus=3), 1)
         self.assertEqual(obj2.get_value(), 8)
+
+    def testFreeze(self):
+
+        def testNotFrozen(obj):
+            self.assertEqual(obj.simple_bool, True)
+            obj.simple_bool = False
+            self.assertEqual(obj.simple_bool, False)
+
+            self.assertEqual(obj.simple_int, 42)
+            obj.simple_int = 18
+            self.assertEqual(obj.simple_int, 18)
+
+            self.assertEqual(obj.simple_str, "dummy")
+            obj.simple_str = "spam"
+            self.assertEqual(obj.simple_str, "spam")
+
+            self.assertEqual(obj.simple_list, [1, 2, 3])
+            obj.simple_list = [2, 4, 6]
+            self.assertEqual(obj.simple_list, [2, 4, 6])
+            obj.simple_list.append(8)
+            self.assertEqual(obj.simple_list, [2, 4, 6, 8])
+            obj.simple_list.pop(0)
+            self.assertEqual(obj.simple_list, [4, 6, 8])
+            del obj.simple_list[:]
+            self.assertEqual(obj.simple_list, [])
+
+            self.assertEqual(obj.simple_tuple, (2, 3, 4))
+            obj.simple_tuple = (4, 6, 8)
+            self.assertEqual(obj.simple_tuple, (4, 6, 8))
+
+            self.assertEqual(obj.simple_dict, {7: 14, 8: 16, 9: 18})
+            obj.simple_dict = {7: 7, 8: 8}
+            self.assertEqual(obj.simple_dict, {7: 7, 8: 8})
+            obj.simple_dict[9] = 9
+            self.assertEqual(obj.simple_dict, {7: 7, 8: 8, 9: 9})
+            obj.simple_dict.pop(8)
+            self.assertEqual(obj.simple_dict, {7: 7, 9: 9})
+            obj.simple_dict.clear()
+            self.assertEqual(obj.simple_dict, {})
+
+            self.assertEqual(obj.simple_set, set([1, 5, 9]))
+            obj.simple_set = set([7, 5, 3])
+            self.assertEqual(obj.simple_set, set([7, 5, 3]))
+            obj.simple_set.add(8)
+            self.assertEqual(obj.simple_set, set([7, 5, 3, 8]))
+            obj.simple_set.remove(5)
+            self.assertEqual(obj.simple_set, set([7, 3, 8]))
+            obj.simple_set.clear()
+            self.assertEqual(obj.simple_set, set())
+
+            self.assertEqual(obj.combined_list, [1, [2, 3], 4])
+            obj.combined_list[1].append(5)
+            self.assertEqual(obj.combined_list, [1, [2, 3, 5], 4])
+            obj.combined_list[1] = 66
+            self.assertEqual(obj.combined_list, [1, 66, 4])
+
+            self.assertEqual(obj.combined_tuple, (1, [2, 3], 4))
+            obj.combined_tuple[1].append(5)
+            self.assertEqual(obj.combined_tuple, (1, [2, 3, 5], 4))
+
+        def testFrozen(obj):
+
+            def set(attr, value):
+                self.assertRaises(AttributeError, setattr, obj, attr, value)
+
+            def raises(fun, *args, **kwargs):
+                self.assertRaises(AttributeError, fun, *args, **kwargs)
+
+            self.assertEqual(obj.simple_bool, True)
+            set("simple_bool", False)
+            self.assertEqual(obj.simple_bool, True)
+
+            self.assertEqual(obj.simple_int, 42)
+            set("simple_int", 18)
+            self.assertEqual(obj.simple_int, 42)
+
+            self.assertEqual(obj.simple_str, "dummy")
+            set("simple_str", "spam")
+            self.assertEqual(obj.simple_str, "dummy")
+
+            self.assertEqual(obj.simple_tuple, (2, 3, 4))
+            set("simple_tuple", (4, 6, 8))
+            self.assertEqual(obj.simple_tuple, (2, 3, 4))
+            self.assertEqual(obj.simple_tuple, (2, 3, 4))
+
+            def check_list(l, expected):
+                # Expected to work
+                self.assertEqual(l, expected)
+                self.assertEqual(l[0], expected[0])
+                self.assertEqual(l[:-1], expected[:-1])
+                self.assertTrue(expected[0] in l)
+
+                # Expected to fail
+                raises(l.append, 8)
+                raises(l.remove, 1)
+                raises(l.pop, 0)
+                raises(l.extend, [6])
+                raises(l.insert, 0, 0)
+                raises(l.reverse)
+                raises(l.sort)
+                raises(l.__delitem__, 0)
+                raises(l.__delslice__, 0, -1)
+                raises(l.__setitem__, 0, None)
+                raises(l.__setslice__, 0, 0, [9])
+
+                # Didn't change
+                self.assertEqual(l, expected)
+
+            check_list(obj.simple_list, [1, 2, 3])
+            set("simple_list", [2, 4, 6])
+            self.assertEqual(obj.simple_list, [1, 2, 3])
+
+            def check_dict(d, expected):
+                # Expected to work
+                self.assertEqual(d, expected)
+                k = expected.keys()[0]
+                self.assertEqual(d[k], expected[k])
+                self.assertEqual(d.get(k), expected[k])
+                self.assertEqual(d.get("NOT IN THERE"), None)
+                self.assertTrue(k in d)
+
+                # Expected to fail
+                raises(d.pop, 8)
+                raises(d.popitem, 8)
+                raises(d.update, {10: 20})
+                raises(d.clear)
+                raises(d.__setitem__, 10, 20)
+                raises(d.__delitem__, 8)
+                self.assertEqual(d, expected)
+
+            check_dict(obj.simple_dict, {7: 14, 8: 16, 9: 18})
+            set("simple_dict", {7: 7, 8: 8})
+            self.assertEqual(obj.simple_dict, {7: 14, 8: 16, 9: 18})
+
+            def check_set(s, expected):
+                # Expected to work
+                self.assertEqual(s, expected)
+                i = list(s)[0]
+                self.assertTrue(i in s)
+
+                # Expected to fail
+                raises(s.pop, 8)
+                raises(s.add, 10)
+                raises(s.update, set([12]))
+                raises(s.remove, 7)
+                raises(s.discard, 7)
+                raises(s.clear)
+                self.assertEqual(s, expected)
+
+            check_set(obj.simple_set, set([1, 5, 9]))
+            set("simple_set", set([4, 5, 6]))
+            self.assertEqual(obj.simple_set, set([1, 5, 9]))
+
+            self.assertEqual(obj.combined_list, [1, [2, 3], 4])
+            check_list(obj.combined_list[1], [2, 3])
+            self.assertEqual(obj.combined_list, [1, [2, 3], 4])
+
+            self.assertEqual(obj.combined_tuple, (1, [2, 3], 4))
+            check_list(obj.combined_tuple[1], [2, 3])
+            self.assertEqual(obj.combined_tuple, (1, [2, 3], 4))
+
+        # test non-frozen as reference
+        obj = TopFreezeDummy()
+        testNotFrozen(obj)
+        testNotFrozen(obj.reference)
+        obj.reference = None
+        self.assertEqual(None, obj.reference)
+        testNotFrozen(obj.list_ref[1])
+        testNotFrozen(obj.tuple_ref[1])
+
+        obj = guarded.freeze(TopFreezeDummy())
+        testFrozen(obj)
+        testFrozen(obj.reference)
+        self.assertRaises(AttributeError, setattr, obj, "reference", None)
+        testFrozen(obj.list_ref[1])
+        testFrozen(obj.tuple_ref[1])
+
+    testFreeze.skip = "Freeze not implemented yet"
