933ff89d90e5e72b92ccf1f1af5b4981a29c9c25
Sebastien Merle <s.merle@gmail.com>
2010-11-26 19:08:43 +0100

    Added replay module and some test for it

--- src/feat/agents/replay.py
+++ src/feat/agents/replay.py
@@ -7,6 +7,11 @@ from feat.interface.journal import *
 @decorator.simple_function
 def mutable(function):
     '''Combined decorator of guarded.mutable and journal.recorded.
+
+    When called from outside a recording context, it returns a Deferred.
+    When called from inside a recording context, it returns a L{fiber.Fiber}
+    or any synchronous value.
+
     Same as using::
 
       @journal.recorded()
@@ -28,18 +33,45 @@ def mutable(function):
 
     return wrapper
 
-# Copy immutable decorator from guarded
+
+@decorator.simple_function
+def entry_point(function):
+    '''Combined decorator of guarded.mutable and journal.recorded
+    that ensure the call is not reentrant.
+
+    If a function decorated with it is called from inside a recording
+    context it will raise L{f.i.j.ReentrantCallError}.
+
+    Because the function cannot be called from inside a recording context,
+    it always returns a Deferred.
+
+    Same as using::
+
+      @journal.recorded(reentrant=False)
+      @guard.mutable
+      def spam(self, state, some, args):
+          pass
+    '''
+
+    fun_id = function.__name__
+    # Register the function
+    annotate.injectClassCallback("recorded", 4,
+                                 "_register_recorded_call",
+                                 fun_id, function)
+
+    def wrapper(self, *args, **kwargs):
+        state = self._get_state()
+        recorder = IRecorder(self)
+        return recorder.call(fun_id, (state, ) + args, kwargs, reentrant=False)
+
+    return wrapper
+
+
+# Copy immutable decorator as-is from guarded module
 immutable = guard.immutable
 
 
-# Fix the metaclass inheritance
-MetaReplayable = type("MetaReplayable", (annotate.MetaAnnotable,
-                                         serialization.MetaSerializable), {})
-
-
 class Replayable(journal.Recorder, guard.Guarded):
-
-#    __metaclass__ = MetaReplayable
 
     def __init__(self, parent, *args, **kwargs):
         journal.Recorder.__init__(self, parent)
--- src/feat/test/test_agents_replay.py
+++ src/feat/test/test_agents_replay.py
@@ -2,31 +2,106 @@
 # -*- Mode: Python -*-
 # vi:si:et:sw=4:sts=4:ts=4
 
+from zope.interface import implements
+
 from feat.agents import replay
+from feat.common import journal, fiber
 
 from feat.interface.serialization import *
+from feat.interface.journal import *
 
 from . import common
 
 
+class DummyJournalKeeper(object):
+
+    implements(IJournalKeeper)
+
+    def __init__(self):
+        self.records = []
+        self.registry = {}
+
+    ### IJournalKeeper Methods ###
+
+    def register(self, recorder):
+        self.registry[recorder.journal_id] = recorder
+
+    def record(self, instance_id, entry_id,
+               fiber_id, fiber_depth, input, side_effects, output):
+        record = (instance_id, entry_id, fiber_id, fiber_depth,
+                  ISnapshot(input).snapshot(),
+                  ISnapshot(side_effects).snapshot(),
+                  ISnapshot(output).snapshot())
+        self.records.append(record)
+
+
 class Base(replay.Replayable):
 
-    def __init__(self, parent, value):
-        replay.Replayable.__init__(self, parent, value)
+    def __init__(self, parent, start_value):
+        replay.Replayable.__init__(self, parent, start_value)
 
-    def init_state(self, state):
-        pass
+    def init_state(self, state, parent, start_value):
+        state.sum = start_value
+
+    @replay.entry_point
+    def sync_main(self, state, value1, value2):
+        return self.sync_add_double(value1) + self.sync_add_double(value2)
 
     @replay.mutable
-    def double(self, state, value, minus=None):
-        pass
+    def sync_add_double(self, state, value, minus=None):
+        result = value * 2 - (minus if minus is not None else 0)
+        state.sum += result
+        return result
+
+    @replay.entry_point
+    def async_main(self, state, value1, value2):
+        f1 = fiber.Fiber()
+        f1.addCallback(self.async_add_double)
+        f1.succeed(value1)
+
+        f2 = fiber.Fiber()
+        f2.addCallback(self.async_add_double)
+        f2.succeed(value2)
+
+        def sum_values(result):
+            return sum([value for success, value in result if success])
+
+        f = fiber.FiberList([f1, f2])
+        f.addCallback(sum_values)
+
+        return f
+
+    @replay.mutable
+    def async_add_double(self, state, value, minus=None):
+        result = value * 2 - (minus if minus is not None else 0)
+        state.sum += result
+        return result
 
     @replay.immutable
-    def get_value(self, state):
-        return state.value
+    def get_sum(self, state):
+        return state.sum
+
+    @replay.entry_point
+    def reentrance_error1(self, state):
+        return self.sync_main(1, 2)
+
+    @replay.mutable
+    def reentrance_error2(self, state):
+        return self.sync_main(1, 2)
 
 
 class TestCombined(common.TestCase):
 
-    def testBasic(self):
-        pass
+    def testSynchronousCalls(self):
+        keeper = journal.InMemoryJournalKeeper()
+        root = journal.RecorderRoot(keeper)
+
+        base = Base(root, 18)
+
+        d = self.assertAsyncEqual(None, 18, base.get_sum)
+        d = self.assertAsyncEqual(d, 24, base.sync_main, 5, 7)
+        d = self.assertAsyncEqual(d, 42, base.get_sum)
+        d = self.assertAsyncEqual(d, 24, base.sync_add_double, 13, minus=2)
+        d = self.assertAsyncEqual(d, 66, base.get_sum)
+
+        return d
