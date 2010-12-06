c212934fa0268790e1cd0d5b1d2d168a1b011c3c
Marek Kowalski <mkowalski@flumotion.com>
2010-12-06 13:25:14 +0100

    Move code common to both agencies from feat.agencies.emu to feat.agencies

--- src/feat/agencies/emu/messaging.py
+++ src/feat/agencies/emu/messaging.py
@@ -2,11 +2,9 @@
 # vi:si:et:sw=4:sts=4:ts=4
 
 from zope.interface import implements
-from twisted.internet import defer, reactor
 from feat.common import log
-from feat.interface.agent import IAgencyAgent
-
-from interface import IConnectionFactory
+from feat.agencies.messaging import Connection, Queue
+from feat.agencies.interface import IConnectionFactory
 
 
 class Messaging(log.Logger, log.FluLogKeeper):
@@ -65,129 +63,6 @@ class Messaging(log.Logger, log.FluLogKeeper):
 
     def _getQueue(self, name):
         return self._queues.get(name, None)
-
-
-class FinishConnection(Exception):
-    pass
-
-
-class Connection(log.Logger):
-
-    log_category = 'messaging-connection'
-
-    def __init__(self, messaging, agent):
-        log.Logger.__init__(self, messaging)
-        self._messaging = messaging
-        self._agent = IAgencyAgent(agent)
-
-        self._queue = self._messaging.defineQueue(
-            self._agent.descriptor.doc_id)
-        self._mainLoop(self._queue)
-        self._bindings = []
-
-    def _mainLoop(self, queue):
-
-        def rebind(_):
-            reactor.callLater(0, bind)
-
-        def stop(reason):
-            self.log('Error handler: exiting, reason %r' % reason)
-
-        def bind():
-            d = self._consumeQueue(queue)
-            d.addCallbacks(rebind, stop)
-
-        bind()
-
-    def _consumeQueue(self, queue):
-
-        def get_and_call_on_message(message):
-            # it is important to always lookup the current message handler
-            # maybe someone bound callback to it ?
-            on_message = self._agent.on_message
-            return on_message(message)
-
-        self._consumeDeferred = queue.consume()
-        self._consumeDeferred.addCallback(get_and_call_on_message)
-        return self._consumeDeferred
-
-    def _append_binding(self, binding):
-        self._bindings.append(binding)
-
-    def _remove_binding(self, binding):
-        self._bindings.remove(binding)
-
-    # IMessagingClient implementation
-
-    def disconnect(self):
-        self._consumeDeferred.errback(FinishConnection("Disconnecting"))
-
-    def personal_binding(self, key, shard=None):
-        if not shard:
-            shard = self._agent.descriptor.shard
-        return PersonalBinding(self, key=key, shard=shard)
-
-    def publish(self, key, shard, message):
-        return self._messaging.publish(key, shard, message)
-
-    def get_bindings(self, shard=None):
-        if shard:
-            return filter(lambda x: x.shard == shard, self._bindings)
-        else:
-            return self._bindings
-
-    # end of IMessagingClient implementation
-
-
-class BaseBinding(object):
-
-    def __init__(self, connection, shard, **kwargs):
-        self._args = kwargs
-        self.connection = connection
-        self.connection._append_binding(self)
-        self.shard = shard
-
-    def revoke(self):
-        self.connection._remove_binding(self)
-
-
-class PersonalBinding(BaseBinding):
-
-    def __init__(self, connection, shard, key, **kwargs):
-        BaseBinding.__init__(self, connection, shard, **kwargs)
-        self.key = key
-        self.exchange = self.connection._messaging.defineExchange(self.shard)
-        self.exchange.bind(self.key, self.connection._queue)
-
-    def revoke(self):
-        self.exchange.unbind(self.key, self.connection._queue)
-        BaseBinding.revoke(self)
-
-
-class Queue(object):
-
-    def __init__(self, name):
-        self.name = name
-        self._messages = []
-
-        self._consumers = []
-
-    def consume(self, *_):
-        d = defer.Deferred()
-        self._consumers.append(d)
-        self._sendMessages()
-
-        return d
-
-    def _sendMessages(self):
-        while len(self._messages) > 0 and len(self._consumers) > 0:
-            message = self._messages.pop(0)
-            consumer = self._consumers.pop(0)
-            consumer.callback(message)
-
-    def enqueue(self, message):
-        self._messages.append(message)
-        reactor.callLater(0, self._sendMessages)
 
 
 class Exchange(object):
