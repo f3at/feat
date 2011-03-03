b09dd56994c72057ff0e4c1e6fd828dd6b680a0e
Marek Kowalski <mkowalski@flumotion.com>
2011-03-03 13:06:02 +0100

    Tests for web component. Simple script mocking out flt cloud and exposing the api.

---
+++ tools/web.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from twisted.internet import reactor
+
+from flt.agents.hapi import dummy, web
+from feat.common import log
+
+
+log.FluLogKeeper.init()
+
+agent = dummy.DummyAgent()
+port = 8800
+server = web.ServerWrapper(agent, port)
+server.start()
+
+print "Listening on http://127.0.0.1:%d" % port
+
+reactor.run()
