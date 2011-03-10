3e57caaac896c739885a7571e48691a0de9f9e03
Xavier Queralt <xqueralt@flumotion.com>
2011-03-10 12:47:39 +0100

    Adding the initial setup script

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+from distutils.command import clean
+
+
+feat_args = dict(name='feat',
+                 version='0.1.1',
+                 description='Flumotion Asynchronous Autonomous Agent Toolkit',
+                 author='Flumotion Developers',
+                 author_email='coreteam@flumotion.com',
+                 platforms=['any'],
+                 package_dir={'': 'src'},
+                 packages=find_packages(where='src', exclude=['flt*']),
+                 scripts=['src/feat/bin/bootstrap.py', 'src/feat/bin/host.py',
+                          'src/feat/bin/standalone.py'],
+                )
+
+flt_args = dict(name='flt',
+                version='0.1.1',
+                description='Flumotion Live Transcoding',
+                author='Flumotion Developers',
+                author_email='coreteam@flumotion.com',
+                platforms=['any'],
+                package_dir={'': 'src'},
+                packages=find_packages(where='src', exclude=['feat*']),
+               )
+
+
+def clean_build(dist):
+    c = clean.clean(dist)
+    c.initialize_options()
+    c.finalize_options()
+    c.all = True
+    c.run()
+
+if os.path.isdir('src/feat'):
+    feat_dist = setup(**feat_args)
+    clean_build(feat_dist)
+
+if os.path.isdir('src/flt'):
+    os.mkdir('build')
+    setup(**flt_args)
