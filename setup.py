d4b4e37f5bc69122cac430ae4fa1eaaca4060ff8
Xavier Queralt <xqueralt@flumotion.com>
2011-03-16 17:03:56 +0100

    Update packaging scripts

--- setup.py
+++ setup.py
@@ -2,44 +2,15 @@
 
 import os
 from setuptools import setup, find_packages
-from distutils.command import clean
 
-
-feat_args = dict(name='feat',
-                 version='0.1.1',
-                 description='Flumotion Asynchronous Autonomous Agent Toolkit',
-                 author='Flumotion Developers',
-                 author_email='coreteam@flumotion.com',
-                 platforms=['any'],
-                 package_dir={'': 'src'},
-                 packages=find_packages(where='src', exclude=['flt*']),
-                 scripts=['src/feat/bin/bootstrap.py', 'src/feat/bin/host.py',
-                          'src/feat/bin/standalone.py'],
-                )
-
-flt_args = dict(name='flt',
-                version='0.1.1',
-                description='Flumotion Live Transcoding',
-                author='Flumotion Developers',
-                author_email='coreteam@flumotion.com',
-                platforms=['any'],
-                package_dir={'': 'src'},
-                packages=find_packages(where='src', exclude=['feat*']),
-               )
-
-
-def clean_build(dist):
-    c = clean.clean(dist)
-    c.initialize_options()
-    c.finalize_options()
-    c.all = True
-    c.run()
-
-if os.path.isdir('src/feat'):
-    feat_dist = setup(**feat_args)
-
-if os.path.isdir('src/flt'):
-    if os.path.isdir('src/feat'):
-        clean_build(feat_dist)
-        os.mkdir('build')
-    setup(**flt_args)
+setup(name='feat',
+      version='0.1.1',
+      description='Flumotion Asynchronous Autonomous Agent Toolkit',
+      author='Flumotion Developers',
+      author_email='coreteam@flumotion.com',
+      platforms=['any'],
+      package_dir={'': 'src'},
+      packages=find_packages(where='src'),
+      scripts=['src/feat/bin/bootstrap.py', 'src/feat/bin/host.py',
+               'src/feat/bin/standalone.py'],
+)
