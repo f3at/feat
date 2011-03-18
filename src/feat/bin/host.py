3d869e347d4f4b8cb8184f241ef5686da5e87eff
Josep Joan Ribas <jribas@flumotion.com>
2011-03-18 15:50:36 +0100

    Fix import, host_agent is now imported in everything

--- src/feat/bin/host.py
+++ src/feat/bin/host.py
@@ -6,5 +6,5 @@ from feat import everything
 if __name__ == '__main__':
     with run.bootstrap() as agency:
         conn = run.get_db_connection(agency)
-        d = conn.save_document(host_agent.Descriptor(shard=u'lobby'))
+        d = conn.save_document(everything.host_agent.Descriptor(shard=u'lobby'))
         d.addCallbacks(agency.start_agent, agency._error_handler)
