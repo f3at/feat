2f7eb8376ba8ecbbdb6e803a0f7978339c89522d
Sebastien Merle <s.merle@gmail.com>
2011-04-01 16:09:34 +0200

    Updates build files and documentation.

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='feat',
-      version='0.1.1',
+      version='0.1.2',
       description='Flumotion Asynchronous Autonomous Agent Toolkit',
       author='Flumotion Developers',
       author_email='coreteam@flumotion.com',
       platforms=['any'],
       package_dir={'': 'src'},
       packages=find_packages(where='src'),
-      scripts=['src/feat/bin/host.py',
+      scripts=['bin/feat',
+               'src/feat/bin/host.py',
                'src/feat/bin/standalone.py'],
 )
