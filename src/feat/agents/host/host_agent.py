fc96296e40fb620ca95099a8719bfa1f3c22ebda
Julien Le Goff <jlegoff@flumotion.com>
2011-04-06 12:12:10 +0200

    Add port allocator in Host Agent

--- src/feat/agents/host/host_agent.py
+++ src/feat/agents/host/host_agent.py
@@ -57,6 +57,7 @@ class HostAgent(agent.BaseAgent, rpc.AgentMixin):
         ports = state.medium.get_descriptor().port_range
         state.port_allocator = port_allocator.PortAllocator(
             self, range(ports[0], ports[1]))
+
         f = fiber.Fiber()
         f.add_callback(fiber.drop_result, self._update_hostname)
         f.add_callback(fiber.drop_result, self._load_definition, hostdef)
@@ -269,7 +270,7 @@ class Descriptor(descriptor.Descriptor):
     # Hostname of the machine, updated when an agent is started
     document.field('hostname', None)
     # Range used for allocating new ports
-    document.field('port_range', (5000, 5999))
+    document.field('port_range', range(5000, 5999))
 
 
 class StartAgentReplier(replier.BaseReplier):
