95148c12760752df7d563315685d07dbefd1154a
Marek Kowalski <mkowalski@flumotion.com>
2011-04-07 18:03:30 +0200

    Add the service discovery protocol.

--- src/feat/agents/base/contractor.py
+++ src/feat/agents/base/contractor.py
@@ -2,6 +2,7 @@ from zope.interface import implements
 from feat.interface import contractor
 from feat.common import log, reflect, serialization
 from feat.agents.base import message, replay
+from feat.interface.protocols import *
 
 
 class Meta(type(replay.Replayable)):
@@ -71,3 +72,40 @@ class BaseContractor(log.Logger, replay.Replayable):
 
     def aborted(self):
         '''@see: L{contractor.IAgentContractor}'''
+
+
+@serialization.register
+class Service(serialization.Serializable):
+    implements(contractor.IContractorFactory)
+
+    def __init__(self, factory):
+        factory = contractor.IContractorFactory(factory)
+        self.protocol_id = 'discover-' + factory.protocol_id
+        self.protocol_type = factory.protocol_type
+        self.initiator = factory.initiator
+        self.interest_type = InterestType.public
+
+    def __call__(self, agent, medium):
+        instance = ServiceDiscoveryContractor(agent, medium)
+        instance.protocol_id = self.protocol_id
+        return instance
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        return self.protocol_id == other.protocol_id and\
+               self.protocol_type == other.protocol_type and\
+               self.initiator == other.initiator and\
+               self.interest_type == other.interest_type
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class ServiceDiscoveryContractor(BaseContractor):
+
+    interest_type = InterestType.public
+
+    @replay.journaled
+    def announced(self, state, announcement):
+        state.medium.bid(message.Bid())
