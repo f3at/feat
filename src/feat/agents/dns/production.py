7a964c0f74760ec6040d42f8eb8eab533b069a68
Josep Joan Ribas <jribas@flumotion.com>
2011-06-16 20:33:45 +0200

    DNS agent can now act as a master dns server.
    We can now configure bind slaves servers that are going to be
    notified on zone updates.
    This will allow configure a bind dns server as slave for the zone.
    See http://www.zytrax.com/books/dns/ch4/#slave

--- src/feat/agents/dns/production.py
+++ src/feat/agents/dns/production.py
@@ -1,10 +1,12 @@
-from twisted.names import client, server, common, dns
-from twisted.python import failure
-from twisted.internet import reactor, error
+import socket
+
+from twisted.names import server, common, dns, authority
+from twisted.python import log
+from twisted.internet import reactor, error, defer
 from zope.interface import implements, classProvides
 
 from feat.agents.base import replay, labour
-from feat.common import defer, serialization
+from feat.common import serialization
 
 from feat.agents.dns.interface import *
 
@@ -15,86 +17,83 @@ class Labour(labour.BaseLabour):
     classProvides(IDNSServerLabourFactory)
     implements(IDNSServerLabour)
 
-    def __init__(self, patron):
+    def __init__(self, patron, resolver, slaves, suffix):
         labour.BaseLabour.__init__(self, patron)
-        self._resolver = Resolver(patron)
+        self._resolver = resolver
         self._listener = None
+        self._tcp_listener = None
+        self._factory = None
+        self._slaves = slaves
+        self._suffix = suffix
 
     @replay.side_effect
     def initiate(self):
-        dns_fact = DNSServerFactory(clients=[self._resolver], verbose=0)
-        udp_fact = dns.DNSDatagramProtocol(dns_fact)
+        self._dns_fact = DNSServerFactory(clients=[self._resolver], verbose=0)
+        udp_fact = dns.DNSDatagramProtocol(self._dns_fact)
         self._factory = udp_fact
 
     @replay.side_effect
     def startup(self, port):
         try:
             self._listener = reactor.listenUDP(port, self._factory)
+            self._tcp_listener = reactor.listenTCP(port, self._dns_fact)
             return True
         except error.CannotListenError:
             return False
 
     def cleanup(self):
-        return self._listener.stopListening()
+        d = defer.maybeDeferred(self._tcp_listener.stopListening)
+        d.addCallback(lambda _: self._listener.stopListening())
+        return d
 
     def get_host(self):
         return self._listener and self._listener.getHost()
 
+    def get_ip(self):
+        return unicode(socket.gethostbyname(socket.gethostname()))
 
-class Resolver(common.ResolverBase):
+    def notify_slaves(self):
+        if self._factory and self._factory.transport:
+            for ip in self._slaves:
+                self._send_notify(ip)
 
-    def __init__(self, patron):
-        common.ResolverBase.__init__(self)
-        self._patron = IDNSServerPatron(patron)
-
-    ### IResolver Methods ###
-
-    def query(self, query, address, timeout=None):
-        '''Interpret and delegate the query to the parent labour.
-        The interface has been modified to take the address as an
-        extra parameter, it need a modified factory to work.'''
-
-        def package_response(response, name, rec_type):
-            results = []
-
-            if not isinstance(response, list):
-                response = [response] if response is not None else []
-
-            for rec in response:
-                record = rec_type(*rec)
-                header = dns.RRHeader(name, record.TYPE, dns.IN,
-                                      record.ttl, record, auth=True)
-                results.append(header)
-
-            if not results:
-                raise dns.DomainError(name)
-
-            return results, [], []
-
-        name = str(query.name)
-
-        if query.type == dns.A:
-            d = defer.succeed(name)
-            d.addCallback(self._patron.lookup_address, address[0])
-            d.addCallback(package_response, name, dns.Record_A)
-            return d
-
-        if query.type == dns.NS:
-            d = defer.succeed(name)
-            d.addCallback(self._patron.lookup_ns)
-            d.addCallback(package_response, name, dns.Record_NS)
-            return d
-
-        return defer.fail(failure.Failure(dns.DomainError(query.name)))
+    def _send_notify(self, address):
+        msg = dns.Message(opCode=dns.OP_NOTIFY)
+        msg.addQuery(self._suffix, type=dns.SOA)
+        self.info('Sending notify to %r', address)
+        self._factory.writeMessage(msg, address)
 
 
 class DNSServerFactory(server.DNSServerFactory):
 
+    def gotResolverError(self, failure, protocol, message, address):
+        '''
+        Copied from twisted.names.
+        Removes logging the whole failure traceback.
+        '''
+        if failure.check(dns.DomainError, dns.AuthoritativeDomainError):
+            message.rCode = dns.ENAME
+        else:
+            message.rCode = dns.ESERVER
+            log.msg(failure.getErrorMessage())
+
+        self.sendReply(protocol, message, address)
+        if self.verbose:
+            log.msg("Lookup failed")
+
     def handleQuery(self, message, protocol, address):
-        '''Copied from twisted.names.
-        Adds passing the address to resolver's query method.'''
+        '''
+        Copied from twisted.names.
+        Adds passing the address to resolver's query method.
+        '''
         query = message.queries[0]
         d = self.resolver.query(query, address)
         d.addCallback(self.gotResolverResponse, protocol, message, address)
         d.addErrback(self.gotResolverError, protocol, message, address)
         return d
+
+    def handleNotify(self, message, protocol, address):
+        '''
+        Not interested in handling notify messages
+        '''
+        pass
