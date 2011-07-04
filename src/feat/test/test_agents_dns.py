d60110f2125acb9f6875f810b4c5b70cc585e4ac
Josep Joan Ribas <jribas@flumotion.com>
2011-07-04 11:34:44 +0200

    Close ports when terminating the dns agent. DNS agent test fixed.

--- src/feat/test/test_agents_dns.py
+++ src/feat/test/test_agents_dns.py
@@ -2,6 +2,7 @@ import socket
 
 from twisted.internet import defer
 from twisted.names import client, dns
+from twisted.names import common as dns_common
 from zope.interface import implements
 
 from feat.agents.dns import dns_agent, production
@@ -12,25 +13,14 @@ from feat.agents.dns.interface import *
 from . import common
 
 
-class DummyPatron(log.LogProxy):
+class TestResolver(dns_agent.Resolver):
 
-    implements(IDNSServerPatron)
-
-    def __init__(self, logger, aa_ttl=300,
-                 ns="ns.mydomain.lan", ns_ttl=300, mapping={}):
-        log.LogProxy.__init__(self, logger)
-        self._aa_ttl = aa_ttl
-        self._mapping = mapping
-        self._ns = ns
-        self._ns_ttl = ns_ttl
-
-    ### IDNSServerPatron Methods ###
-
-    def lookup_address(self, name, address):
-        return [(ip, self._aa_ttl) for ip in self._mapping.get(name, [])]
-
-    def lookup_ns(self, name):
-        return self._ns, self._ns_ttl
+    def __init__(self, suffix='mydomain.lan', ns_ttl=300):
+        self.suffix = suffix
+        ns = 'ns.'+suffix
+        host_ip='127.0.0.1'
+        notify = dns_agent.NotifyConfiguration()
+        dns_agent.Resolver.__init__(self, suffix, ns, notify, host_ip, ns_ttl)
 
 
 class TestDNSAgent(common.TestCase):
@@ -39,16 +29,21 @@ class TestDNSAgent(common.TestCase):
     def testNSQueries(self):
 
         @defer.inlineCallbacks
-        def check(name, exp_ns, exp_ttl, **kwargs):
-            patron = DummyPatron(self, **kwargs)
-            labour = production.Labour(patron)
+        def check(exp_ns, exp_ttl, **kwargs):
+            resolver = TestResolver(**kwargs)
+            patron = log.LogProxy(self)
+            labour = production.Labour(patron,
+                                       resolver,
+                                       slaves=[],
+                                       suffix=resolver.suffix)
             labour.initiate()
             res = labour.startup(0)
             self.assertTrue(res)
             address = labour.get_host()
             port = address.port
-            resolver = client.Resolver(servers=[("127.0.0.1", port)])
-            res = yield resolver.queryUDP([dns.Query(name, dns.NS)])
+            cresolver = client.Resolver(servers=[("127.0.0.1", port)])
+            res = yield cresolver.queryUDP(
+                [dns.Query(resolver.suffix, dns.NS)])
             self.assertEqual(len(res.answers), 1)
             answer = res.answers[0]
             self.assertEqual(answer.ttl, exp_ttl)
@@ -57,24 +52,32 @@ class TestDNSAgent(common.TestCase):
             self.assertEqual(payload.name.name, exp_ns)
             yield labour.cleanup()
 
-        yield check("spam", "ns.mydomain.lan", 300)
-        yield check("spam", "ns.mydomain.lan", 42, ns_ttl=42)
-        yield check("spam", "spam.lan", 300, ns="spam.lan")
+        yield check("ns.mydomain.lan", 300)
+        yield check("ns.mydomain.lan", 42, ns_ttl=42)
+        yield check("ns.spam.lan", 300, suffix="spam.lan")
 
     @defer.inlineCallbacks
     def testAQueries(self):
 
         @defer.inlineCallbacks
-        def check(name, exp_ips, exp_ttl, **kwargs):
-            patron = DummyPatron(self, **kwargs)
-            labour = production.Labour(patron)
+        def check(name, exp_ips, exp_ttl, mapping, aa_ttl=300):
+            resolver = TestResolver()
+            for name, ips in mapping.items():
+                map(lambda ip: resolver.add_record(name, resolver.suffix, ip,
+                                                    aa_ttl), ips)
+            name = name +'.'+resolver.suffix
+            patron = log.LogProxy(self)
+            labour = production.Labour(patron,
+                                       resolver,
+                                       slaves=[],
+                                       suffix=resolver.suffix)
             labour.initiate()
             res = labour.startup(0)
             self.assertTrue(res)
             address = labour.get_host()
             port = address.port
-            resolver = client.Resolver(servers=[("127.0.0.1", port)])
-            res = yield resolver.queryUDP([dns.Query(name, dns.A)])
+            cresolver = client.Resolver(servers=[("127.0.0.1", port)])
+            res = yield cresolver.queryUDP([dns.Query(name, dns.A)])
             result = []
             for answer in res.answers:
                 self.assertEqual(answer.ttl, exp_ttl)
@@ -84,10 +87,10 @@ class TestDNSAgent(common.TestCase):
             self.assertEqual(exp_ips, result)
             yield labour.cleanup()
 
-        yield check("spam.lan", [], 300)
-        yield check("spam.lan", ["192.168.0.1"], 300,
-                    mapping={"spam.lan": ["192.168.0.1"]})
-        yield check("spam.lan", ["192.168.0.1"], 42, aa_ttl=42,
-                    mapping={"spam.lan": ["192.168.0.1"]})
-        yield check("spam.lan", ["192.168.0.1", "192.168.0.2"], 300,
-                    mapping={"spam.lan": ["192.168.0.1", "192.168.0.2"]})
+        yield check("spam", [], 300, {})
+        yield check("spam", ["192.168.0.1"], 300,
+                    {"spam": ["192.168.0.1"]})
+        yield check("spam", ["192.168.0.1"], 42,
+                    {"spam": ["192.168.0.1"]}, aa_ttl=42)
+        yield check("spam", ["192.168.0.1", "192.168.0.2"], 300,
+                    {"spam": ["192.168.0.1", "192.168.0.2"]})
