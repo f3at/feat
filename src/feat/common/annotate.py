ffaa426387ec1612a6d8890f596da414539d105b
Marek Kowalski <mkowalski@flumotion.com>
2011-08-23 12:46:58 +0200

    Fix yet another bug with mixins vs annotations vs mro registries.

--- src/feat/common/annotate.py
+++ src/feat/common/annotate.py
@@ -2,6 +2,7 @@ from feat.common import reflect
 
 _CLASS_ANNOTATIONS_ATTR = "_class_annotations"
 _ATTRIBUTE_INJECTIONS_ATTR = "_attribute_injections"
+_ANNOTATIONS_PROCESSED = "_annotations_processed"
 
 
 class AnnotationError(Exception):
@@ -11,7 +12,7 @@ class AnnotationError(Exception):
 class MetaAnnotable(type):
 
     def __init__(cls, name, bases, dct):
-        klasses = [cls] + list(cls.mro())
+        klasses = list(reversed(cls.mro()))
 
         # Class Initialization
         method = getattr(cls, "__class__init__", None)
@@ -20,28 +21,34 @@ class MetaAnnotable(type):
 
         # Attribute Injection
         for k in klasses:
-            injections = getattr(k, _ATTRIBUTE_INJECTIONS_ATTR, None)
+            injections = k.__dict__.get(_ATTRIBUTE_INJECTIONS_ATTR, None)
             if injections is not None:
                 for attr, value in injections:
                     setattr(k, attr, value)
                 del injections[:]
 
+        pending_annotations = list()
         # Class Annotations
         for k in klasses:
-            annotations = getattr(k, _CLASS_ANNOTATIONS_ATTR, None)
+            if k.__dict__.get(_ANNOTATIONS_PROCESSED, False):
+                continue
+            is_annotable = issubclass(type(k), MetaAnnotable)
+            annotations = k.__dict__.get(_CLASS_ANNOTATIONS_ATTR, None)
             if annotations is not None:
-                for name, methodName, args, kwargs in annotations:
-                    method = None
-                    for k2 in klasses:
-                        method = getattr(k2, methodName, None)
-                        if method is not None:
-                            break
-                    if method is None:
-                        raise AnnotationError("Bad annotation %s set on class "
-                                              "%s, method %s not found"
-                                              % (name, k, methodName))
-                    method(*args, **kwargs)
-                del annotations[:]
+                if is_annotable:
+                    to_process = pending_annotations + annotations
+                    for name, methodName, args, kwargs in to_process:
+                        method = getattr(k, methodName, None)
+                        if method is None:
+                            raise AnnotationError(
+                                "Bad annotation %s set on class "
+                                "%s, method %s not found"
+                                % (name, k, methodName))
+                        method(*args, **kwargs)
+                    pending_annotations = list()
+                    setattr(k, _ANNOTATIONS_PROCESSED, True)
+                else:
+                    pending_annotations.extend(annotations)
 
         super(MetaAnnotable, cls).__init__(name, bases, dct)
 
