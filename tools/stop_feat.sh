22127d69fa645c3ebb9bb39ac2497c714138ed96
Sebastien Merle <s.merle@gmail.com>
2011-09-09 11:15:54 +0200

    Fixes stop_feat.sh error message because of normal ssh disconnection.

--- tools/stop_feat.sh
+++ tools/stop_feat.sh
@@ -7,11 +7,14 @@ PIDFILE="$RUNDIR/feat.pid"
 if [ -e $PIDFILE ]; then
     echo "Shutting down FEAT..."
     ssh localhost -p 6000 "shutdown()"
+    exit_code=$?
     if [ $? -ne 0 ]; then
-    	PID=$(cat $PIDFILE)
-    	echo "Error while shutting down process $PID"
-    	echo "Removing $PIDFILE..."
-    	rm $PIDFILE > /dev/null 2>&1
+        if [ -e $PIDFILE ]; then
+            PID=$(cat $PIDFILE)
+            echo "Error while shutting down process $PID"
+            echo "Removing $PIDFILE..."
+            rm $PIDFILE > /dev/null 2>&1
+        fi
     fi
 else
     echo "PID file not found."
