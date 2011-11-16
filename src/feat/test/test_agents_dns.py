c06ab6392f3e1baf118d6b1b954ca9795dae725f
Andoni Morales Alastruey <amorales@flumotion.com>
2011-11-16 16:29:45 +0100

    Add unit tests for the dns agent

--- src/feat/test/test_agents_dns.py
+++ src/feat/test/test_agents_dns.py
@@ -24,15 +24,17 @@ import socket
 from twisted.internet import defer
 from twisted.names import client, dns
 
+from feat.agents.base import resource, message
 from feat.agents.dns import dns_agent, production
-from feat.common import log
+from feat.common import log, guard
+from feat.test.dummies import DummyMedium, DummyCache
 
 from feat.agents.dns.interface import RecordA, RecordCNAME
 
 from . import common
 
 
-class TestDNSAgent(common.TestCase):
+class TestDNSAgentLabour(common.TestCase):
 
     @defer.inlineCallbacks
     def testNSQueries(self):
@@ -136,6 +138,110 @@ class TestDNSAgent(common.TestCase):
         yield check("spam", ["192.168.0.1"], 300)
         yield check("spam", ["192.168.0.1"], 42, aa_ttl=42)
         yield check("spam", ["192.168.0.1", "192.168.0.2"], 300)
+
+
+class TestDNSAgentMisc(common.TestCase):
+
+    def testDnsName(self):
+        self.assertEquals(
+            dns_agent.DnsName.name_to_id('test'), 'dns_test')
+        self.assertEquals(
+            dns_agent.DnsName.id_to_name('dns_guainch'), 'guainch')
+        self.failUnlessRaises(AttributeError,
+            dns_agent.DnsName.id_to_name, 'jau')
+
+    def testView(self):
+        name = {'_id': '1', '.type': 'dns_name',
+                'zone': 'test.lan', 'name': 'jander'}
+        view = dns_agent.DnsView()
+
+        self.assertEquals(list(view.map(name))[0], (name['zone'], name['_id']))
+        query1 = {'query': {'zone': 'test.lan'}}
+        query2 = {'query': {'zone': 'test1.lan'}}
+        self.assertTrue(view.filter(name, query1))
+        self.assertFalse(view.filter(name, query2))
+
+
+class DummyDNSMedium(DummyMedium):
+
+    def get_configuration(self):
+        return dns_agent.DNSAgentConfiguration(suffix='lan')
+
+    def descriptor_class(self):
+        desc = dns_agent.Descriptor()
+        desc.resources = {'dns': resource.AllocatedRange([53])}
+        return desc
+
+
+@common.attr('slow', timeout=40)
+class TestDNSAgent(common.TestCase):
+
+    @defer.inlineCallbacks
+    def setUp(self):
+        self.medium = DummyDNSMedium(self)
+        self.dns = dns_agent.DNSAgent(self.medium)
+        self.collector = \
+            dns_agent.MappingUpdatesCollector(self.dns, self.medium)
+        self.cache = DummyCache(self.dns)
+
+        yield self.dns.initiate()
+        yield self.dns.startup()
+        self.dns._get_state().cache = self.cache
+        self.dns._get_state().document_updater = self.cache
+
+    def _notify(self, action, *args):
+        msg = message.BaseMessage(payload=[action, args])
+        self.collector.notified(msg)
+
+    @defer.inlineCallbacks
+    def testMappings(self):
+        # Add mapping
+        yield self.dns.add_mapping('test', '1.1.1.1')
+        # Resolve mapping
+        a = yield self.dns.lookup_address('test.lan', '')
+        self.assertEquals(a[0][0], '1.1.1.1')
+        # Add duplicated mapping
+        yield self.dns.add_mapping('test', '1.1.1.1')
+        # Remove mapping
+        yield self.dns.remove_mapping('test', '1.1.1.1')
+        a = yield self.dns.lookup_address('test.lan', '')
+        # Remove again the mapping
+        yield self.dns.remove_mapping('test', '1.1.1.1')
+        # Remove wrong mapping
+        yield self.dns.remove_mapping('test', '1.1.1.2')
+        self.assertEquals(a, [])
+
+    @defer.inlineCallbacks
+    def testAlias(self):
+        # Add alias
+        yield self.dns.add_alias('dog', '1.1.1.2')
+        # Resolve alias
+        a = yield self.dns.lookup_alias('dog.lan')
+        self.assertEquals(a[0], '1.1.1.2')
+        # Remove alias
+        yield self.dns.remove_alias('dog', '1.1.1.2')
+        a = yield self.dns.lookup_alias('dog')
+        # Remove alias again
+        yield self.dns.remove_alias('dog', '1.1.1.2')
+        self.assertEquals(a, (None, None))
+        # Add same alias for different ips
+        yield self.dns.add_alias('dog', '1.1.1.2')
+        yield self.dns.add_alias('dog', '1.1.1.1')
+
+    def testSuffix(self):
+        self.assertTrue(self.dns.get_suffix(), 'lan')
+
+    def testNS(self):
+        self.assertTrue(self.dns.lookup_ns('test.lan'), '')
+
+    def testUpdates(self):
+        defer.setDebugging=True
+        self._notify('add_mapping', 'test', '1.1.1.1')
+        self._notify('remove_mapping', 'test', '1.1.1.1')
+        self._notify('add_alias', 'test', 'dog')
+        self._notify('remove_alias', 'test', 'dog')
+        # Try with an uknown action
+        self._notify('remove_beach', 'test', 'dog')
 
 
 def format_name(prefix, suffix):
