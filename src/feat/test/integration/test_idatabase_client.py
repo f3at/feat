3626cfe6b19ac8e2b4a531657a0da4bccaf708bb
Sebastien Merle <s.merle@gmail.com>
2011-12-15 15:55:04 +0100

    Fixes emu database when saving a document previously deleted.

--- src/feat/test/integration/test_idatabase_client.py
+++ src/feat/test/integration/test_idatabase_client.py
@@ -104,6 +104,22 @@ class CountingView(view.BaseView):
 class TestCase(object):
 
     @defer.inlineCallbacks
+    def testSavingDeletedDoc(self):
+        doc1 = DummyDocument(field=u'something')
+        doc1 = yield self.connection.save_document(doc1)
+        fetched_doc1 = yield self.connection.get_document(doc1.doc_id)
+        self.assertEqual(u'something', fetched_doc1.field)
+        yield self.connection.delete_document(fetched_doc1)
+        yield self.asyncErrback(NotFoundError,
+                                 self.connection.get_document, doc1.doc_id)
+        doc2 = DummyDocument(field=u'someone')
+        doc2.doc_id = doc1.doc_id
+        doc2 = yield self.connection.save_document(doc2)
+        self.assertEqual(doc1.doc_id, doc2.doc_id)
+        fetched_doc2 = yield self.connection.get_document(doc1.doc_id)
+        self.assertEqual(u'someone', fetched_doc2.field)
+
+    @defer.inlineCallbacks
     def testQueryingViews(self):
         # create design document
         views = (SummingView, CountingView, )
