449c75789879bf1aee7a14e479742f9e46a0d019
Marek Kowalski <mkowalski@flumotion.com>
2012-01-03 16:13:51 +0100

    Add info line informing the the logging will be done to temporary file during the agency startup.

--- src/feat/agencies/bootstrap.py
+++ src/feat/agencies/bootstrap.py
@@ -194,6 +194,8 @@ class _Bootstrap(object):
             log.FluLogKeeper.set_debug(self.opts.debug)
         if self.opts.agency_daemonize:
             tmp = tempfile.mktemp(suffix="feat.temp.log")
+            log.info("run", "Until we agency figures out it's role logging "
+                     "will be done to the file: %s", tmp)
             run.daemonize(stdout=tmp, stderr=tmp)
         self.agency = self._run_agency()
         return self
