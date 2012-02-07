d55d18e4cfff1e41638b128de9f590ee72150445
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-02-07 05:52:05 -0500

    Fix stop script's exit code handling.
    Don't remove pid file; a permission denied leaves feat running and deleting the pid file makes things harder to fix later.

--- tools/stop_feat.sh
+++ tools/stop_feat.sh
@@ -8,12 +8,15 @@ if [ -e $PIDFILE ]; then
     echo "Shutting down FEAT..."
     ssh localhost -p 6000 "shutdown()"
     exit_code=$?
-    if [ $? -ne 0 ]; then
+    if [ $exit_code -ne 0 ]; then
         if [ -e $PIDFILE ]; then
             PID=$(cat $PIDFILE)
             echo "Error while shutting down process $PID"
-            echo "Removing $PIDFILE..."
-            rm $PIDFILE > /dev/null 2>&1
+            echo "Consider removing $PIDFILE..."
+            # rm $PIDFILE > /dev/null 2>&1
+        else
+            echo "Could not shutdown through ssh, and $PIDFILE not found."
+            exit $exit_code
         fi
     fi
 else
