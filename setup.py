eea8b6053a34c99f6a7fee75190124ad4ced0c57
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-03-13 17:20:29 +0100

    bump version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 
 
 NAME = 'feat'
-VERSION = '0.16'
+VERSION = '0.18.0'
 DESCRIPTION = 'Flumotion Asynchronous Autonomous Agent Toolkit'
 LONG_DESC = DESCRIPTION
 AUTHOR = 'Flumotion Developers',
