02965bbcb703f2e20166e9de85a0f81a30cc47e9
Marek Kowalski <mkowalski@flumotion.com>
2012-03-28 13:06:50 +0200

    Define versioned formatable and make paisley serializer capable of promoting the types to versioned.

--- src/feat/common/formatable.py
+++ src/feat/common/formatable.py
@@ -24,6 +24,7 @@
 import copy
 
 from feat.common import serialization, annotate
+from feat.common.serialization.json import VERSION_ATOM
 
 
 class Field(object):
@@ -119,3 +120,13 @@ class Formatable(serialization.Serializable, annotate.Annotable):
             else:
                 value = copy.copy(field.default)
             setattr(self, field.name, value)
+
+
+class VersionedFormatable(Formatable):
+
+    version = 1
+
+    def snapshot(self):
+        snapshot = Formatable.snapshot(self)
+        snapshot[str(VERSION_ATOM)] = type(self).version
+        return snapshot
