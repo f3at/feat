8acb2256b081b630d7acbe10e0d36a28ae1c3aee
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-11-12 12:37:42 +0100

    release 0.23.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 
 NAME = 'feat'
 
-VERSION = '0.23.2'
+VERSION = '0.23.3'
 DESCRIPTION = 'Flumotion Asynchronous Autonomous Agent Toolkit'
 LONG_DESC = DESCRIPTION
 AUTHOR = 'Flumotion Developers',
