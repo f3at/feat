141499d304cda35f97faacca1a2fc8c59a6db357
Marek Kowalski <kowalski0123@gmail.com>
2012-12-14 13:49:08 +0100

    Define basic callables to be used with IDatabaseClient.update_document().

---
+++ src/feat/database/update.py
@@ -0,0 +1,57 @@
+from feat.database.interface import ResignFromModifying
+
+
+def attributes(document, params):
+    changed = False
+    for key, value in params.iteritems():
+        if getattr(document, key) != value:
+            setattr(document, key, value)
+            changed = True
+    if changed:
+        return document
+    else:
+        raise ResignFromModifying()
+
+
+def append_to_list(document, key, value, unique=False):
+    changed = False
+    llist = getattr(document, key)
+    if not isinstance(llist, list):
+        setattr(document, [value])
+        changed = True
+    elif not (unique and value in llist):
+        llist.append(value)
+        changed = True
+    if changed:
+        return document
+    else:
+        raise ResignFromModifying()
+
+
+def extend_list(document, key, values):
+    llist = getattr(document, key)
+    if not isinstance(llist, list):
+        setattr(document, values)
+    else:
+        list.extend(values)
+    return document
+
+
+def step(method, *args, **kwargs):
+    return (method, args, kwargs)
+
+
+def steps(document, *updates):
+    changed = True
+    for method, args, kwargs in updates:
+        try:
+            document = method(document, *args, **kwargs)
+            if document is None:
+                return
+            changed = True
+        except ResignFromModifying:
+            pass
+    if changed:
+        return document
+    else:
+        raise ResignFromModifying()
