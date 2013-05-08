cb6ae99aaace3183162352dee65dd6d1563ac0df
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-05-08 17:51:01 +0200

    Add test that really tests for port

--- src/feat/test/test_common_connstr.py
+++ src/feat/test/test_common_connstr.py
@@ -12,24 +12,33 @@ class ConnStrTest(common.TestCase):
         self.assertEqual('ftp', resp['protocol'])
         self.assertEqual(None, resp['user'])
         self.assertEqual(None, resp['password'])
-        self.assertEqual(None, resp['port'])
         self.assertEqual('fff.sss.ggg', resp['host'])
+        self.assertEqual(None, resp['port'])
 
     def testPostgres(self):
         resp = connstr.parse('postgres://feat:feat@encoder001.fff.sss.ggg')
         self.assertEqual('postgres', resp['protocol'])
         self.assertEqual('feat', resp['user'])
         self.assertEqual('feat', resp['password'])
-        self.assertEqual(None, resp['port'])
         self.assertEqual('encoder001.fff.sss.ggg', resp['host'])
+        self.assertEqual(None, resp['port'])
+
+    def testPostgresPort(self):
+        resp = connstr.parse('postgres://feat:feat@encoder001.fff.sss.ggg:123')
+        self.assertEqual('postgres', resp['protocol'])
+        self.assertEqual('feat', resp['user'])
+        self.assertEqual('feat', resp['password'])
+        self.assertEqual('encoder001.fff.sss.ggg', resp['host'])
+        self.assertEqual(123, resp['port'])
+
 
     def testSQLite(self):
         resp = connstr.parse('sqlite:///var/log/journal.sqlite3')
         self.assertEqual('sqlite', resp['protocol'])
         self.assertEqual(None, resp['user'])
         self.assertEqual(None, resp['password'])
-        self.assertEqual(None, resp['port'])
         self.assertEqual('/var/log/journal.sqlite3', resp['host'])
+        self.assertEqual(None, resp['port'])
 
     def testSQLiteDash(self):
         resp = connstr.parse(
@@ -38,6 +47,6 @@ class ConnStrTest(common.TestCase):
         self.assertEqual('sqlite', resp['protocol'])
         self.assertEqual(None, resp['user'])
         self.assertEqual(None, resp['password'])
-        self.assertEqual(None, resp['port'])
         self.assertEqual('/home/thomas/credex/git/dev/release-1.5.x/'
             'feat/log/journal.sqlite3', resp['host'])
+        self.assertEqual(None, resp['port'])
