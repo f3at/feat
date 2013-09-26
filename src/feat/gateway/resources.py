cf989babdbd7b7c599bbee6ed48c2adeb2aa5113
Marek Kowalski <kowalski0123@gmail.com>
2013-09-26 16:54:41 +0200

    Fix parsing arguments of POST to idempotent action of retrieve category.

--- src/feat/gateway/resources.py
+++ src/feat/gateway/resources.py
@@ -542,11 +542,11 @@ class ActionResource(BaseResource):
             or self._action.category not in categories):
             raise http.NotAllowedError(allowed_methods=list(self._methods))
 
-        # in case of ActionCategory.retrieve action, the action parameters
-        # are in the query string, here we parse it
+        # in case GET request to the ActionCategory.retrieve action,
+        # the action parameters are in the query string, here we parse it
         request.debug("%r", self._action.category)
         if (self._action.category == ActionCategories.retrieve and
-            self._action.is_idempotent):
+            self._action.is_idempotent and request.method == http.Methods.GET):
             params = dict()
             request.debug("%r", self._action.parameters)
             request.debug("%r", request.arguments)
