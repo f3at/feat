8de47c59752b19a84e67d6b17f326b97a986188d
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2014-05-09 09:12:36 -0400

    new prerelease

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 
 NAME = 'feat'
 
-VERSION = '0.29.2'
+VERSION = '0.29.3'
 DESCRIPTION = 'Flumotion Asynchronous Autonomous Agent Toolkit'
 LONG_DESC = DESCRIPTION
 AUTHOR = 'Flumotion Developers',
